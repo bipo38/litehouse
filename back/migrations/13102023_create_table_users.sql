26c0617edda871d1b5487db2881cd240cc2aa614
bipo38ls <bipoben7@gmail.com>
2023-10-13 20:56:15 +0200

    feat: add user migration

---
+++ back/migrations/13102023_create_table_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users(
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       name VARCHAR(25) NOT NULL,
+       password varchar(30) NOT NULL,
+       email nvarchar(255) NOT NULL,
+       created_at DATETIME NOT NULL
+)
