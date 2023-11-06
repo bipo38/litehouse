8b60e40ad87a003da15a18de1eeea493f6724c38
bipo38 <bipoben7@gmail.com>
2023-11-06 20:30:23 +0000

    feat: home

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,7 +1,18 @@
 <template>
-    <div class="page--home">
-        <h1>Working 🔨</h1>
-    </div>
+    <main class="page--home">
+
+        <div class="page--home__intro">
+            <div>
+                <h1>LiteHouse </h1>
+                <img src="~/assets/svg/light.svg" alt="">
+            </div>
+            <p>Lighthouse in a simple way</p>
+        </div>
+        <div class="page--home__login-btns">
+            <button class="button--primary button--big" aria-label="Sign in button">Sign in</button>
+            <button class="button--outline button--big" aria-label="Sign up button">Sign up</button>
+        </div>
+    </main>
 </template>
 
 <style lang="scss">
