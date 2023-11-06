76f3c6a36b502825ffcec934a985f91e7ed79125
bipo38 <bipoben7@gmail.com>
2023-11-06 19:33:06 +0000

    refactor: buttons

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,16 +1,6 @@
 <template>
     <div class="page--home">
         <h1>Working 🔨</h1>
-        <button class="button--primary button">Hola</button>
-        <button class="button--secondary button">Hola</button>
-
-        <h1>Hola</h1>
-        <h2>Hola</h2>
-        <h3>Hola</h3>
-        <h4>Hola</h4>
-        <p>Hola </p>
-        <HomeReportView />
-
     </div>
 </template>
 
