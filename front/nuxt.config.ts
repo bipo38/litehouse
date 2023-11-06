05dca16a5f8586ff2486f70ecd7b5572e8a085da
Bipo <104152564+bipo38@users.noreply.github.com>
2023-11-06 20:33:32 +0100

    feat: styles init (#6)
    * feat: styles init
    
    * refactor: colors
    
    * feat: secondary btn
    
    * feat:  defaut layout
    
    * refactor: buttons

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: false },
+  css: ["~/assets/styles/main.scss"],
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      Inter: {
+        wght: [300, 400, 500, 700, 900],
+      },
+    },
+  },
+});
---
+++ front/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="page--home">
+        <h1>Working 🔨</h1>
+    </div>
+</template>
+
+<style lang="scss">
+@use '../assets/styles/pages/home.scss'
+</style>
