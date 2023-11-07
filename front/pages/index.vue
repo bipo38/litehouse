9afac582d5f7b7d9f0656c5140dec9429ee0505d
Bipo <104152564+bipo38@users.noreply.github.com>
2023-11-07 19:38:19 +0100

    feat: home (#7)
    * feat: home
    
    * refactor: wip
    
    * refactor: home styles

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,6 +1,17 @@
 <template>
-    <div class="page--home">
-        <h1>Working 🔨</h1>
+    <div class="page page--home">
+
+        <div class="page--home__intro">
+            <div>
+                <h1>LiteHouse </h1>
+                <img src="~/assets/svg/light.svg" alt="">
+            </div>
+            <p>Lighthouse in a simple way</p>
+        </div>
+        <div class="page--home__login-btns">
+            <nuxt-link to="/" class="button button--primary button--big" aria-label="Sign in button">Sign in</nuxt-link>
+            <nuxt-link to="/" class="button button--outline button--big" aria-label="Sign up button">Sign up</nuxt-link>
+        </div>
     </div>
 </template>
 
