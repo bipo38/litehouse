67ec67496f555b524bc5c5b265edcedd648d28fb
bipo38 <bipoben7@gmail.com>
2023-11-11 18:27:20 +0100

    feat: eslint

--- front/pages/signin.vue
+++ front/pages/signin.vue
@@ -1,45 +1,45 @@
 <script setup lang="ts">
 definePageMeta({
-    layout: 'welcome'
+  layout: 'welcome'
 })
-
 
 const email = ref('')
 const password = ref('')
 
-let dataRes : Ref<response | null> = ref(null);
-
+const dataRes : Ref<response | null> = ref(null)
 
 const login = async () => {
-dataRes.value = await useApi().post("/api/login", {
-        body: {
-            email: email.value,
-            password: password.value,
-        },
-    })
+  dataRes.value = await useApi().post('/api/login', {
+    body: {
+      email: email.value,
+      password: password.value
+    }
+  })
 }
 
 </script>
 
 <template>
-    <div class="page--login">
+  <div class="page--login">
+    <h2>Sign in</h2>
+    <form @submit.prevent="login">
+      <input v-model="email" class="input--default" type="email" required placeholder="Email">
+      <input v-model="password" class="input--default" type="password" required placeholder="Password">
 
-        <h2>Sign in</h2>
-        <form @submit.prevent="login">
-            <input class="input--default" type="email" required placeholder="Email" v-model="email">
-            <input class="input--default" type="password" required placeholder="Password" v-model="password">
-
-            <div v-if="dataRes?.ok "  >
-                {{ dataRes.data }}     
-            </div>
-
-            <button class="button--primary">Sign in</button>
-        </form>
-        <div class="page--login__links">
-            <nuxt-link to="/signup" class="link--underline link--small">Not registered yet?</nuxt-link>
-            <nuxt-link to="/" class="link--underline link--small">Forgot password?</nuxt-link>
-        </div>
-
-        
+      <div v-if="dataRes?.ok ">
+        {{ dataRes.data }}
+      </div>
+      <button class="button--primary">
+        Sign in
+      </button>
+    </form>
+    <div class="page--login__links">
+      <nuxt-link to="/signup" class="link--underline link--small">
+        Not registered yet?
+      </nuxt-link>
+      <nuxt-link to="/" class="link--underline link--small">
+        Forgot password?
+      </nuxt-link>
     </div>
+  </div>
 </template>
