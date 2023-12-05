e89d6da712cabee42923fa49ac10ea8dcc27b387
bipo38 <bipoben7@gmail.com>
2023-12-05 15:56:43 +0100

    wip

--- front/components/Pages/PageCard.vue
+++ front/components/Pages/PageCard.vue
@@ -12,7 +12,15 @@ const urls: Array<PageUrl> = JSON.parse(props.page.urls)
 <template>
   <div class="page-card">
     <div class="page-card__header">
-      <p>{{ page.title }}</p>
+      <div class="page-card__title">
+        <MiscIconLoader name="caret-down" />
+        <p>{{ page.title }}</p>
+      </div>
+      <div class="page-card__edit">
+        <NuxtLink :to="`/pages/${page.page_id}`">
+          <MiscIconLoader name="edit" />
+        </NuxtLink>
+      </div>
     </div>
   </div>
 </template>
